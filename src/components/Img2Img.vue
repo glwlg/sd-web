3b24099b1d7c26e0d0c0dc94f2ef0d864dfa1dfd
guoluwei <651971564@qq.com>
2023-11-18 13:55:40 +0800

    样式调整

--- src/components/Img2Img.vue
+++ src/components/Img2Img.vue
@@ -36,13 +36,16 @@ export default {
     const loading = ref(false)
 
     const onUploadClick = () => {
+      resultImage.value = null;
+      originalImage.value = null;
+      fileInput.value.value = null;
       fileInput.value.click()
     }
 
     const img2img = () => {
       axios.post('/sd/sdapi/v1/img2img', {
-        "prompt": "masterpiece, best quality, glasses,looking away,  (wide-eyed:1.2),1boy,realistic, 3d,",
-        "negative_prompt": "(NSFW:2),(sexy:2),(cross-eyed:1.5),EasyNegative, drawn by bad-artist, sketch by bad-artist-anime, (bad_prompt:0.8), (artist name, signature, watermark:1.4), (ugly:1.2), (worst quality, poor details:1.4), bad-hands-5, badhandv4, blurry, blush, red_face",
+        "prompt": "masterpiece, best quality,realistic, 3d,attractive",
+        "negative_prompt": "(cross-eyed:1.5),EasyNegative,wrinkled skin,  drawn by bad-artist, sketch by bad-artist-anime, (bad_prompt:0.8), (artist name, signature, watermark:1.4), (ugly:1.2), (worst quality, poor details:1.4), bad-hands-5, badhandv4, blurry, blush, red_face",
         // "styles": [
         //     "string"
         // ],
@@ -50,54 +53,54 @@ export default {
           originalImage.value
         ],
         "steps": 30,
-        "cfg_scale": 5,
+        "cfg_scale": 10,
         "seed": -1,
-        "refiner_checkpoint": "realcartoon3d_v30.safetensors",
+        "refiner_checkpoint": "",
+        "refiner_switch_at": 0.8,
         "width": 512,
         "height": 704,
         "denoising_strength": 0.35,
-        "subseed": -1,
-        "subseed_strength": 0,
-        "seed_resize_from_h": -1,
-        "seed_resize_from_w": -1,
-        "sampler_name": "Euler a",
-        "batch_size": 1,
-        "n_iter": 1,
-        "restore_faces": true,
-        "tiling": true,
-        "do_not_save_samples": false,
-        "do_not_save_grid": false,
-        "eta": 0,
-        "s_min_uncond": 0,
-        "s_churn": 0,
-        "s_tmax": 0,
-        "s_tmin": 0,
-        "s_noise": 0,
-        "override_settings": {},
-        "override_settings_restore_afterwards": true,
-        "refiner_switch_at": 0,
-        "disable_extra_networks": false,
-        "comments": {},
-        "resize_mode": 2,
         "image_cfg_scale": 1.5,
-        // "mask": "string",
-        "mask_blur_x": 4,
-        "mask_blur_y": 4,
-        "mask_blur": 0,
-        "inpainting_fill": 0,
-        "inpaint_full_res": true,
-        "inpaint_full_res_padding": 0,
-        "inpainting_mask_invert": 0,
-        "initial_noise_multiplier": 0,
-        // "latent_mask": "string",
-        // "force_task_id": "string",
-        "sampler_index": "Euler",
+        "sampler_name": "DPM++ 2M Karras",
         "include_init_images": false,
-        // "script_name": "string",
-        // "script_args": [],
         "send_images": true,
         "save_images": false,
-        "alwayson_scripts": {}
+        "do_not_save_samples": true,
+        "do_not_save_grid": true,
+        "disable_extra_networks": true,
+        // "subseed": -1,
+        // "subseed_strength": 0,
+        // "seed_resize_from_h": -1,
+        // "seed_resize_from_w": -1,
+        // "sampler_index": "DPM",
+        // "batch_size": 1,
+        // "n_iter": 1,
+        // "restore_faces": true,
+        // "tiling": true,
+        // "eta": 0,
+        // "s_min_uncond": 0,
+        // "s_churn": 0,
+        // "s_tmax": 0,
+        // "s_tmin": 0,
+        // "s_noise": 0,
+        // "override_settings": {},
+        // "override_settings_restore_afterwards": true,
+        // "comments": {},
+        // "resize_mode": 2,
+        // "mask": "string",
+        // "mask_blur_x": 4,
+        // "mask_blur_y": 4,
+        // "mask_blur": 0,
+        // "inpainting_fill": 0,
+        // "inpaint_full_res": true,
+        // "inpaint_full_res_padding": 0,
+        // "inpainting_mask_invert": 0,
+        // "initial_noise_multiplier": 0,
+        // "latent_mask": "string",
+        // "force_task_id": "string",
+        // "script_name": "string",
+        // "script_args": [],
+        // "alwayson_scripts": {}
       })
           .then(function (response) {
             // console.log(JSON.stringify(response.data));
@@ -113,7 +116,6 @@ export default {
       if (file) {
         const reader = new FileReader()
         reader.onload = (e) => {
-          resultImage.value = null;
           originalImage.value = e.target.result
           loading.value = true;
           img2img();
