9de8ca4f426ce2b53418e0434acf30ccd47482e1
guoluwei <651971564@qq.com>
2023-11-21 16:45:40 +0800

    修改样式

--- src/components/Img2Img.vue
+++ src/components/Img2Img.vue
@@ -36,8 +36,9 @@
           aria-modal="true"
       >
         <n-space justify="space-between">
-          <div class="svgBox" style="background-color: #6c8be5" @click="img2img('3dboy')">
-            <svg style="width: 40px;color: #ffffff;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          <div class="svgBox" style="background-color: #6c8be5" @click="img2img('3dboy2')">
+            <svg style="width: 40px;color: #ffffff;" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink"
                  viewBox="0 0 1024 1024">
               <path
                   d="M874 120H622c-3.3 0-6 2.7-6 6v56c0 3.3 2.7 6 6 6h160.4L583.1 387.3c-50-38.5-111-59.3-175.1-59.3c-76.9 0-149.3 30-203.6 84.4S120 539.1 120 616s30 149.3 84.4 203.6C258.7 874 331.1 904 408 904s149.3-30 203.6-84.4C666 765.3 696 692.9 696 616c0-64.1-20.8-124.9-59.2-174.9L836 241.9V402c0 3.3 2.7 6 6 6h56c3.3 0 6-2.7 6-6V150c0-16.5-13.5-30-30-30zM408 828c-116.9 0-212-95.1-212-212s95.1-212 212-212s212 95.1 212 212s-95.1 212-212 212z"
@@ -46,16 +47,15 @@
           </div>
           <div class="svgBox"
                style="background-color: #ffa8a8"
-               @click="img2img('3dboy')">
-            <svg style="width: 40px;color: #ffffff;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+               @click="img2img('3dgirl2')">
+            <svg style="width: 40px;color: #ffffff;" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink"
                  viewBox="0 0 1024 1024">
               <path
                   d="M712.8 548.8c53.6-53.6 83.2-125 83.2-200.8c0-75.9-29.5-147.2-83.2-200.8C659.2 93.6 587.8 64 512 64s-147.2 29.5-200.8 83.2C257.6 200.9 228 272.1 228 348c0 63.8 20.9 124.4 59.4 173.9c7.3 9.4 15.2 18.3 23.7 26.9c8.5 8.5 17.5 16.4 26.8 23.7c39.6 30.8 86.3 50.4 136.1 57V736H360c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h114v140c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V812h114c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8H550V629.5c61.5-8.2 118.2-36.1 162.8-80.7zM512 556c-55.6 0-107.7-21.6-147.1-60.9C325.6 455.8 304 403.6 304 348s21.6-107.7 60.9-147.1C404.2 161.5 456.4 140 512 140s107.7 21.6 147.1 60.9C698.4 240.2 720 292.4 720 348s-21.6 107.7-60.9 147.1C619.7 534.4 567.6 556 512 556z"
                   fill="currentColor"></path>
             </svg>
           </div>
-          <!--          <n-button @click="img2img('3dboy')">帅哥</n-button>-->
-          <!--          <n-button @click="img2img('3dgirl')">美女</n-button>-->
         </n-space>
       </n-card>
     </n-modal>
@@ -65,107 +65,101 @@
 import {defineComponent, ref} from 'vue'
 import axios from "axios";
 import imageCompression from 'browser-image-compression';
+import Img2imgConfig from '../model/Img2imgConfig';
 
 export default defineComponent({
-  setup() {
-    const fileInput = ref(null)
-    const originalImage = ref(null)
-    const resultImage = ref(null)
-    const loading = ref(false)
-    const showStyleModel = ref(false)
-    const style = ref(null)
+      setup() {
+        const fileInput = ref(null)
+        const originalImage = ref(null)
+        const resultImage = ref(null)
+        const loading = ref(false)
+        const showStyleModel = ref(false)
+        const style = ref(null)
+        const width = ref(512)
+        const height = ref(704)
 
-    const onUploadClick = () => {
-      resultImage.value = null;
-      originalImage.value = null;
-      fileInput.value.value = null;
-      fileInput.value.click()
-    }
-
-    const img2img = (chooseStyle) => {
-      style.value = chooseStyle;
-      loading.value = true;
-      showStyleModel.value = false;
-      axios.post('/sd/sdapi/v1/img2img', {
-        "styles": [
-          style.value
-        ],
-        "init_images": [
-          originalImage.value
-        ],
-        "steps": 30,
-        "cfg_scale": 10,
-        "seed": 1142528603,
-        "refiner_checkpoint": "chibi_V1.safetensors",
-        "refiner_switch_at": 0.6,
-        "width": 512,
-        "height": 704,
-        "denoising_strength": 0.3,
-        "image_cfg_scale": 0,
-        "sampler_name": "DPM++ 2M SDE Heun Karras",
-        "include_init_images": true,
-        "send_images": true,
-        "save_images": false,
-        "do_not_save_samples": true,
-        "do_not_save_grid": true,
-        "disable_extra_networks": true,
-        "resize_mode": 0,
-      })
-          .then(function (response) {
-            // console.log(JSON.stringify(response.data));
-            resultImage.value = 'data:image/png;base64,' + response.data.images[0];
-            loading.value = false;
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
-    const onFileChange = async (event) => {
-      const file = event.target.files[0]
-      if (file) {
-        const options = {
-          maxSizeMB: 1,          // (default: Number.POSITIVE_INFINITY)
-          maxWidthOrHeight: 1024,  // (default: undefined)
-          useWebWorker: true,      // (default: true)
-          maxIteration: 10,       // (default: 10)
-          exifOrientation: true,  // (default: false)
-          onProgress: undefined, // (default: null) hook function to log progress
-          initialQuality: 0.8    // (default: 0.92)
+        const img2img = (chooseStyle) => {
+          style.value = chooseStyle;
+          loading.value = true;
+          showStyleModel.value = false;
+          var imgConfig = new Img2imgConfig(style.value, originalImage.value);
+          imgConfig.width = width.value
+          imgConfig.height = height.value
+          axios.post('/sd/sdapi/v1/img2img', imgConfig)
+              .then(function (response) {
+                resultImage.value = 'data:image/png;base64,' + response.data.images[0];
+                loading.value = false;
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
         }
-        try {
-          const compressedFile = await imageCompression(file, options);
-          const reader = new FileReader()
-          reader.onload = (e) => {
-            originalImage.value = e.target.result
-            showStyleModel.value = true;
+
+        const onUploadClick = () => {
+          resultImage.value = null;
+          originalImage.value = null;
+          fileInput.value.value = null;
+          fileInput.value.click()
+        }
+        const onFileChange = async (event) => {
+          const file = event.target.files[0]
+          if (file) {
+            const options = {
+              maxSizeMB: 1,          // (default: Number.POSITIVE_INFINITY)
+              maxWidthOrHeight: 1024,  // (default: undefined)
+              useWebWorker: true,      // (default: true)
+              maxIteration: 10,       // (default: 10)
+              exifOrientation: true,  // (default: false)
+              onProgress: undefined, // (default: null) hook function to log progress
+              initialQuality: 0.8    // (default: 0.92)
+            }
+            try {
+              const compressedFile = await imageCompression(file, options);
+              const reader = new FileReader()
+              reader.onload = (e) => {
+                let img = new Image();
+                img.onload = function () {
+                  console.log('Image Width: ' + this.width + ', Image Height: ' + this.height);
+                  if (this.width <= 512) {
+                    width.value = this.width;
+                    height.value = this.height;
+                  } else {
+                    height.value = width.value / this.width * this.height;
+                  }
+                  originalImage.value = e.target.result;
+                  showStyleModel.value = true;
+                }
+                img.src = e.target.result;
+              }
+              reader.readAsDataURL(compressedFile)
+            } catch (error) {
+              console.log(error);
+            }
           }
-          reader.readAsDataURL(compressedFile)
-        } catch (error) {
-          console.log(error);
+        }
+
+        const onContinueClick = () => {
+          document.body.scrollTop = document.documentElement.scrollTop = 0;
+          originalImage.value = resultImage.value;
+          resultImage.value = null;
+          img2img(style.value);
+        }
+
+        return {
+          fileInput,
+          originalImage,
+          resultImage,
+          loading,
+          showStyleModel,
+          onUploadClick,
+          onContinueClick,
+          onFileChange,
+          img2img
         }
       }
     }
-
-    const onContinueClick = () => {
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-      originalImage.value = resultImage.value;
-      resultImage.value = null;
-      img2img(style.value);
-    }
-
-    return {
-      fileInput,
-      originalImage,
-      resultImage,
-      loading,
-      showStyleModel,
-      onUploadClick,
-      onContinueClick,
-      onFileChange,
-      img2img
-    }
-  }
-});
+)
+;
 </script>
 
 <style scoped>
---
+++ src/model/Img2imgConfig.js
@@ -0,0 +1,32 @@
+import ControlNet from './ControlNet.js';
+
+export default class Img2imgConfig {
+    constructor(style, originalImage) {
+        this.styles = [style];
+        this.init_images = [originalImage];
+        this.steps = 30;
+        this.cfg_scale = 10;
+        this.seed = -1;
+        this.refiner_checkpoint = "";
+        this.refiner_switch_at = "0";
+        this.width = 512;
+        this.height = 704;
+        this.denoising_strength = 0.3,
+        this.image_cfg_scale = 0,
+        this.sampler_name = "DPM++ 2M SDE Heun Karras",
+        this.include_init_images = true,
+        this.send_images = true,
+        this.save_images = false,
+        this.do_not_save_samples = true,
+        this.do_not_save_grid = true,
+        this.disable_extra_networks = true,
+        this.resize_mode = 1,
+            this.alwayson_scripts = {
+                controlnet: {
+                    args: [
+                        new ControlNet()
+                    ]
+                }
+            };
+    }
+}
